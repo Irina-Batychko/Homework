a3bdfa082a65469ca082b244627d6e2ed856c6be
Ирина <116366603+Irina-Batychko@users.noreply.github.com>
2023-08-25 16:15:42 +0300

    Task_1.py
    Внесены изменения согласно замечанию преподавателя.

--- Homework_3/Task_1.py
+++ Homework_3/Task_1.py
@@ -19,3 +19,22 @@ def remove_duplicates(input_list):
 input_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7]
 result = remove_duplicates(input_list)
 print(result)
+
+
+
+Исправлено согласно замечанию:
+
+def remove_duplicates(input_list):
+    unique_elements = set()
+    result_list = []
+
+    for item in input_list:
+        if item in unique_elements and item not in result_list:
+            result_list.append(item)
+        unique_elements.add(item)
+
+    return result_list
+
+input_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7]
+result = remove_duplicates(input_list)
+print(result)
